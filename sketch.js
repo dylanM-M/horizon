00d26a272f9cab0396c6dd18b24c564c527a1285
dylanM-M <dylan.marelicmcintyre@gmail.com>
2025-08-27 12:30:28 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 // === Canvas settings ===
-const CANVAS_W = 1600;
-const CANVAS_H = 1000;
+const CANVAS_W = 160;
+const CANVAS_H = 100;
 let margin = 9;
 let numRects = 8;
 let rectWidth = (CANVAS_W - margin * (numRects + 1)) / numRects;
